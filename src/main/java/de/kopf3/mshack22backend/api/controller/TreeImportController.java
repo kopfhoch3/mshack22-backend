e0de013a520b02a514e90f9aea48a53e145b7fed
Atgoogat <36623640+Atgoogat@users.noreply.github.com>
2022-09-23 20:40:42 +0200

    add: mongo support

--- src/main/java/de/kopf3/mshack22backend/api/controller/TreeImportController.java
+++ src/main/java/de/kopf3/mshack22backend/api/controller/TreeImportController.java
@@ -1,14 +1,45 @@
 package de.kopf3.mshack22backend.api.controller;
 
+import de.kopf3.mshack22backend.persitence.document.ActivityPoint;
+import de.kopf3.mshack22backend.persitence.document.TreePoint;
+import de.kopf3.mshack22backend.persitence.repository.ActivityPointRepository;
+import de.kopf3.mshack22backend.persitence.repository.TreePointRepository;
+import de.kopf3.mshack22backend.wstos.Tree;
 import de.kopf3.mshack22backend.wstos.TreeWrapper;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.Timestamp;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequiredArgsConstructor
 public class TreeImportController {
 
+    private final TreePointRepository treePointRepository;
+
     @PostMapping("treeimport")
-    public void importGeoJson(@RequestBody TreeWrapper treeWrapper) {
+    public List<TreePoint> importGeoJson(@RequestBody TreeWrapper treeWrapper) {
+        final var treeDocuments = treeWrapper.getFeatures().stream()
+            .map(this::createTreePoint)
+            .toList();
+        return this.treePointRepository.saveAll(treeDocuments);
+    }
+
+    private TreePoint createTreePoint(final Tree tree) {
+        final TreePoint tp = new TreePoint();
+        tp.setTitle("Baum");
+        tp.setDescription("Baum");
+        tp.setTreeFamily(tree.getProperties().getBaumgruppe());
+        tp.setResolved(false);
+        tp.setLocation(new GeoJsonPoint(tree.getGeometry().getX(), tree.getGeometry().getY()));
+        tp.setTimestamp(Timestamp.from(ZonedDateTime.now().toInstant()));
+        return tp;
     }
 }
---
+++ src/main/java/de/kopf3/mshack22backend/persitence/document/ActivityPoint.java
@@ -0,0 +1,30 @@
+package de.kopf3.mshack22backend.persitence.document;
+
+import java.time.ZonedDateTime;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivityPoint {
+    @MongoId
+    private ObjectId id;
+
+    @GeoSpatialIndexed(type = GeoSpatialIndexType.GEO_2DSPHERE)
+    private GeoJsonPoint location;
+    private String title;
+    private String description;
+    private ZonedDateTime timestamp;
+    private boolean resolved;
+}
