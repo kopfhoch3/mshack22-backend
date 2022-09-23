0ba01f2be48055aeca4eab19a8e2aa6627c40442
Atgoogat <36623640+Atgoogat@users.noreply.github.com>
2022-09-23 12:42:43 +0200

    Inital commit

---
+++ src/main/java/de/kopf3/mshack22backend/Mshack22BackendApplication.java
@@ -0,0 +1,13 @@
+package de.kopf3.mshack22backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mshack22BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mshack22BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/kopf3/mshack22backend/Mshack22BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kopf3.mshack22backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mshack22BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
